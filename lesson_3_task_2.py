8474f51c6676ac3d43d3781601ec2c793bec4acf
Anton <sovinskyi.anton.gmail.com>
2022-08-27 00:01:23 +0300

    Full name greeting

--- lesson_3_task_2.py
+++ lesson_3_task_2.py
@@ -1,3 +1,11 @@
+# Full name greeting
+
+first_name = "anton"
+last_name = "sovinskyi"
+print("Hello,", first_name.title() + ' ' + last_name.title() + "!", "Have a good day!")
+
+# Another way of full name greeting
+
 first_name = "anton"
 last_name = "sovinskyi"
 full_name = f"{first_name} {last_name}"
