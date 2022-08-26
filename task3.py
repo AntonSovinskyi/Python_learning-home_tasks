242463c684bab1785643938fb5db93d85ab78968
Anton <sovinskyi.anton.gmail.com>
2022-08-26 22:24:16 +0300

    Printing OH

---
+++ task3.py
@@ -0,0 +1,25 @@
+"""Printing OH by using one and multiple print() with adding '\n' and '\t' symbols"""
+
+
+print("#########")
+print("#\t\t#")
+print("#\t\t#")
+print("#\t\t#")
+print("#########\n")
+
+print("#\t\t#")
+print("#\t\t#")
+print("#########")
+print("#\t\t#")
+print("#\t\t#\n")
+
+print("#########\n"
+      "#\t\t#\n"
+      "#\t\t#\n"
+      "#\t\t#\n"
+      "#########\n\n"
+      "#\t\t#\n"
+      "#\t\t#\n"
+      "#########\n"
+      "#\t\t#\n"
+      "#\t\t#\n")
