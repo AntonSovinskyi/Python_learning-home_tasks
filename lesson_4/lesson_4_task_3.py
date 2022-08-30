0fb5c00ad457b74258ecfe73bcf9a33494ef54f9
Anton <sovinskyi.anton.gmail.com>
2022-08-30 11:56:17 +0300

    answer for a mathematical expression

---
+++ lesson_4/lesson_4_task_3.py
@@ -0,0 +1,7 @@
+a, b, c = 2, 3, 2
+d = (a + b) ** c
+user = input('Enter the result of a mathematical expression: ')
+if int(user) != d:
+    print(f'{user} is incorrect. Have a cup of tea, an try again later.')
+else:
+    print(f'Are you doctor of sciences? {user} is correct answer!')
