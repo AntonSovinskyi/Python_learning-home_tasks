91455fa856ed51dc378df3656fd3641862e69bc6
Anton <sovinskyi.anton.gmail.com>
2022-08-30 13:25:30 +0300

    sep=, end=

--- lesson_2/lesson_2_task_2.py
+++ lesson_2/lesson_2_task_2.py
@@ -5,17 +5,17 @@
 print("Hello Beetroot!")
 
 # Fun greeting, using sep=
-print("H", "e" ,"l" ,"l" ,"o", "_" 
+print("H", "e", "l", "l", "o", "_"
       "B", "e", "e", "t", "r", "o", "o", "t" "!",
       sep="_")
 
 # Fun greeting, using sep= with line break
-print("H", "e" ,"l" ,"l" ,"o", "\n" 
-      "B", "e", "e", "t", "r", "o", "o", "t" "!",
+print("H", "e", "l", "l", "o", "\n" 
+      "B", "e", "e", "t", "r", "o", "o", "t", "!",
       sep="_")
 
-# Simple messege with using end=
+# Simple message with using end=
 print("Coding is fun", end="!!!\n")
 
 # Message with using commas
-print(1,4, 7, 14, 21)
+print(1, 4, 7, 14, 21)
