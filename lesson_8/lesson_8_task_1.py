57a379dc2f752b7e462eaff5dc3b18a2a6a71476
Anton <sovinskyi.anton.gmail.com>
2022-09-07 15:38:41 +0300

    Favorite movie

---
+++ lesson_8/lesson_8_task_1.py
@@ -0,0 +1,6 @@
+"""Simple function, which takes a string containing the name of favorite movie."""
+
+def favorite_movie(movie):
+    print(f'My favorite movie is named "{movie}".')
+
+favorite_movie('City of Angels')
