f96a03a5fd664ec5aac53749f964cf4a9faf994f
Anton <sovinskyi.anton.gmail.com>
2022-09-07 17:33:06 +0300

    Creating a dictionary

--- lesson_8/lesson_8_task_2.py
+++ lesson_8/lesson_8_task_2.py
@@ -1,3 +1,6 @@
+# Не дуже зрозумів завдання, тому зробив декілька варіантів.
+# Чи є серед них вірний?)))
+
 """Creating a function, which takes in a country's name and capital as parameters.
 Then creating a dictionary from those two, with 'name' as a key and 'capital' as a parameter.
 Make the function print out the values of the dictionary to make sure that it works as intended.
@@ -11,3 +14,34 @@ def make_country(country, capital):
 
 
 print(make_country('Ukraine', 'Kyiv'))
+
+
+# Інший варіант
+
+def make_country(**kwargs):
+    return kwargs
+
+
+print(make_country(country='Ukraine', capital='Kyiv'))
+
+
+# Ще варіант
+
+country = str(input('Print a country: '))
+capital = str(input("Print it's capital: "))
+
+def make_country(**kwargs):
+    return kwargs
+
+
+print(make_country(country=country, capital=capital))
+
+
+# І ще один
+def make_country(**kwargs):
+    for key, value in kwargs.items():
+        kwargs = dict(kwargs)
+        return kwargs
+
+
+print(make_country(country='Ukraine', capital='Kyiv'))
