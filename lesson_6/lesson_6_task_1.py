4fb4333915510f350d52348d910c5c3336ab1662
Anton <sovinskyi.anton.gmail.com>
2022-09-10 17:04:32 +0300

    The greatest number

---
+++ lesson_6/lesson_6_task_1.py
@@ -0,0 +1,18 @@
+""" A program to get the largest number from a list
+ of random numbers with the length of 10
+
+
+ """
+
+
+import random
+
+
+num = []
+n = 10
+while len(num) < n:
+    num.append(random.randint(1, 1000))
+
+print(num)
+max_item = max(num)
+print(max_item)
