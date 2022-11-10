e0062275adbd4108111403dbbb285a37bcca2587
Anton <sovinskyi.anton.gmail.com>
2022-11-10 18:19:17 +0200

    UDP

---
+++ Home_tasks/lessonn_36/task_1(client).py
@@ -0,0 +1,15 @@
+"""Create a server and client, which will use user datagram protocol (UDP) for communication."""
+
+
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        message = input('Enter message: ')
+        if message == 'exit':
+            break
+        s.sendto(message.encode('ascii'), (HOST, PORT))
